aaa28198728b017dd23b0276fb73d1a0f5b0a0fd
jealouscloud <github@noaha.org>
2025-07-13 11:12:17 -0400

    Homepage improvements

--- backend/web/hypertext/pages.py
+++ backend/web/hypertext/pages.py
@@ -6,16 +6,27 @@ from . import blog, common
 
 
 def home():
+    def br2():
+        return ht.br(), ht.br()
+
     return common.create(
         content=ht.section[
             ht.article(class_="o-card")[
                 ht.div["Welcome to the Hypertext World!"],
                 ht.span[
-                    "I'm Noah Ablaseau, and this is my personal website.",
+                    "I'm Noah, and this is ",
+                    ht.s["my favorite site on the citadel"],
+                    " my personal website.",
+                    br2(),
+                    "It's a bit of a work in progress. "
+                    "I'm trying to avoid frameworks while solving the problems that traditional frameworks solve. ",
                     ht.br(),
-                    ht.br(),
+                    "To that end, this site is built with ",
+                    ht.a(href="https://github.com/jealouscloud/html-compose")[
+                        "html-compose"
+                    ],
+                    ", a declarative way to build HTML from Python.",
                 ],
-                ht.a(href="/blog")["Go to the Blog"],
             ]
         ]
     )
