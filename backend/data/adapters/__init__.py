fa02e9a2f6427f02d1709b50ae33efac5852d59c
jealouscloud <github@noaha.org>
2025-07-30 21:18:56 -0400

    Update docstring

--- backend/data/adapters/__init__.py
+++ backend/data/adapters/__init__.py
@@ -5,7 +5,15 @@ from sqlalchemy import CursorResult
 
 def param_check(result: CursorResult, named_tuple: type[NamedTuple]):
     """
-    raise ValueError if result columns
+    This is a deceivingly simple check to ensure that the database query
+    result matches the data type we expect to marshal it to.
+
+    When querying, you can use .label() to rename columns in the query
+    to match the NamedTuple fields.
+
+    :param result: The SQLAlchemy CursorResult object from the query.
+    :param named_tuple: The NamedTuple type to check against.
+    :raises ValueError: If the result columns do not match the NamedTuple fields.
     """
     keys = tuple(result.keys())
     if keys != named_tuple._fields:
