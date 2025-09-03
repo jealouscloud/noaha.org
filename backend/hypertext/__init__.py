7946641470b16e0eee099e451434f505fee9942d
jealouscloud <github@noaha.org>
2025-09-02 20:12:41 -0400

    Add 'local' script concept for page locality

--- backend/hypertext/__init__.py
+++ backend/hypertext/__init__.py
@@ -8,12 +8,19 @@ def create(*args, **kwargs):
     """
     Create a complete HTML document with the given content and other parameters.
     """
+    local_scripts = kwargs.pop("local_scripts", [])
+    local_css = kwargs.pop("local_css", [])
     doc = HTML5Document(
         title="Noah Ablaseau",
         head=[
             e.script(src=cache_bust("/public/bundle.js")),
             e.script(src=cache_bust("/public/vanillajs/index.js")),
+            *[e.script(src=cache_bust(script)) for script in local_scripts],
             e.link(rel="stylesheet", href=cache_bust("/public/css/main.css")),
+            *[
+                e.link(rel="stylesheet", href=cache_bust(css))
+                for css in local_css
+            ],
             e.link(
                 rel="icon",
                 href=cache_bust("/public/assets/favicon.ico"),
